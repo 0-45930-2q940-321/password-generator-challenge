f0c61e8cb3293a927b6261746329c61383beae18
906284 <michaelcounseller@gmail.com>
2021-11-20 13:48:42 -0600

    made the variables global to be able to assign them in functions, fixed click function

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ const num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '/', '|', '{', '}', '_', '.', '`', '~']
 
+//The variables need to be declared globally so you can put it into several functions. And then we can assign it into whatever you want.
+var addLowerCase, addUpperCase, addSpecialChar, addNumbers, getPasswordLength;
+
 //When 'Generate Password' button is clicked it'll start several prompts to register the length of the password, minimum of 8 max of 128 characters, as well as to include; special characters, numbers, upper or lower case letters
 function passwordCreationInfo() {
-  //parseInt will return the entered string as an integer
-  var getPasswordLength = parseInt(prompt('Enter a number to generate a length! Must be between 8 ~ 128'));
+    //parseInt will return the entered string as an integer
+    getPasswordLength = parseInt(prompt('Enter a number to generate a length! Must be between 8 ~ 128'));
 
   if (isNaN(getPasswordLength)) {
     alert('You must enter a number! Don\'t you know how to count?😡');
@@ -23,20 +26,31 @@ function passwordCreationInfo() {
     alert('I said it has to be between 8 and 128! 🙄')
     passwordCreationInfo();
   }
+    //Can short hand 'window.confirm' to just 'confirm'
+
     //Asking if to add Upper Case to the password generation
-    var addLowerCase = confirm('Lets start with some choices for your password, want some LOWERCASES? 😎');
+    addLowerCase = confirm('Lets start with some choices for your password, want some LOWERCASES? 😎');
+    if(!addLowerCase) {
+      alert('Respectable.. I like uppercases more 🤫')
+    }
 
     //Asking if to add Lower Case to the password generation
-    var addUpperCase = confirm('UPPERCASES is better than lowercases and who doesn\'t like having the best 🥰');
-
+    addUpperCase = confirm('UPPERCASES is better than lowercases and who doesn\'t like having the best 🥰');
+    if(!addUpperCase) {
+      alert('Wow, what are you scared of UPPERCASES?! BOO! 👻')
+    }
 
     //Asking if to add Numbers to the password generation
-    var addNumbers = confirm('NUMBERS are pretty neat lets grab some 😜');
-
+    addNumbers = confirm('NUMBERS are pretty neat lets grab some 😜');
+    if(!addNumbers) {
+      alert('I guess you won\'t have the winning lottery ticket🤑')
+    }
 
     //Asking to add special Characters to the password generation
-    var addSpecialChar = confirm('Want some super secret SPECIAL CHARACTERS? 😈');
-
+    addSpecialChar = confirm('Want some super secret SPECIAL CHARACTERS? 😈');
+    if(!addSpecialChar) {
+      alert('Fine I guess we won\'t have COOL characters 😧')
+    }
 
     //If all variables return false it will not count
     if (!addUpperCase && !addLowerCase && !addNumbers && !addSpecialChar) {
@@ -44,10 +58,14 @@ function passwordCreationInfo() {
       alert('MINIMUM OF 1 CHOICE NEEDED! And it\'s not gonna be good if you don\'t have atleast 3 🤔');
       return;
     }
+    //When the 'Generate Password' is clicked it will start 'passwordCreationInfo' function will also call 'writePassword' function
+    writePassword();
 }
+
 
 //Passing a parameter called 'fourArrays' to get a random number/character from an array with a set length.
 function getRandomCharacter(fourArrays) {
+
   let randomCharacterOrNumber = Math.floor(Math.random() * fourArrays.length);
   let selectedCharacter = fourArrays[randomCharacterOrNumber];
 
@@ -79,8 +97,9 @@ function generatePassword() {
     selectedChoices = selectedChoices.concat(specialChar);
   }
 
-  //Making a quick variable to push the random characters/numbers we get. What we get is dependent on which window.confirm is true.
-  for (let i = 0; i < getPasswordLength.length; i++) {
+  //Making a quick variable to push the random characters/numbers we get. What we get is dependent on which 'confirm' is true.
+  //Don't need '.length' because its already a number 
+  for (let i = 0; i < getPasswordLength; i++) {
     let p = getRandomCharacter(selectedChoices);
 
     createPassword.push(p);
@@ -101,4 +120,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordCreationInfo);
